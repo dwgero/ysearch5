d1d13770761e9c3147794c4c58c87873e89dc074
David Gero <dwgero@gmail.com>
2026-08-22 16:26:51 -0400

    Initial Commit

---
+++ ysearch5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ysearch5
+//
+//  Created by David Gero on 8/22/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}

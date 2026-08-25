8b0896c8d8d838832beef471913e77322f05bda0
David Gero <dwgero@gmail.com>
2026-08-25 00:09:17 -0400

    Add GPL notices to source files

--- ysearch5/main.c
+++ ysearch5/main.c
@@ -1,9 +1,21 @@
-//
-//  ysearch5
-//
-//  Created by David W. Gero on 8/22/26.
-//  Copyright (C) 2026 by David W. Gero.  All Rights Reserved.
-//
+/*
+ * main.c
+ * part of ysearch5
+ * Copyright (C) 2026 by David W. Gero.  All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #if defined(__APPLE__)
 #define _DARWIN_C_SOURCE
